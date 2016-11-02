35663e1891e19b48878bc917e537efef1f65d13e
Liz Howard <liz.howard@galvanize.com>
2016-11-02 12:41:26 -0700

    Turned page into a class, attached response object to page for deeper tests

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ A simple, opinionated library for writing integration tests with Superagent and 
 
 ### Description
 
-This library is maintained for use by Galvanize instructors and curriculum developers to test student exercise code. It's designed to test simple applications written by junior developers by automating user actions, such as filling in a form and clicking a submit button. 
+This library is maintained for use by Galvanize instructors and curriculum developers to test student exercise code. It's designed to test simple applications written by junior developers by automating user actions, such as filling in a form and clicking a submit button.
 
 ## Installation
 
@@ -17,19 +17,143 @@ npm install galvanize-superjsdom
 
 Include it in your test script, and it will attach itself to the global scope.
 
-* `createPage(request)` - Takes a superagent request object, and returns a chainable promise interface
+<a name="Page"></a>
 
-### Chainable methods on createPage
+## Page
+**Kind**: global class  
 
-- `.visit(path)` - Sends an initial `GET` request to a page
-- `.validate(input)` - Validates the page is W3C standards compliant
-- `.get(path)` - sends a `GET` request
-- `.post(path, controls)` - sends a `POST` request
-- `.fillIn(text, options)` - Fills in an `input` or `textarea` with text, based on the content of it's `label` element
-- `.check(text)` - Checks a checkbox based on the content of it's `label` element
-- `.select(text, options)` - Selects a specific `option` from a `select` based on it's text
-- `.clickLink(text)` - Finds a `<a>` based on the text inside the tag (case sensitive)
-- `.clickButton(text)` - Clicks a `input[type=submit]` button (only) with the specified text, then gathers the nearest form values into a `POST` and sends it.
+* [Page](#Page)
+    * [new Page(request)](#new_Page_new)
+    * [.visit(path)](#Page+visit) ⇒ <code>Promise</code>
+    * [.validate($)](#Page+validate) ⇒ <code>Promise</code>
+    * [.get(path)](#Page+get) ⇒ <code>Promise</code>
+    * [.post(path, controls)](#Page+post) ⇒ <code>type</code>
+    * [.clickLink(text)](#Page+clickLink) ⇒ <code>Promise</code>
+    * [.fillIn(text, options)](#Page+fillIn) ⇒ <code>function</code>
+    * [.check(text)](#Page+check) ⇒ <code>function</code>
+    * [.clickButton(text)](#Page+clickButton) ⇒ <code>Promise</code>
+    * [.select(text, options)](#Page+select) ⇒ <code>function</code>
+
+<a name="new_Page_new"></a>
+
+### new Page(request)
+Represents a Page.
+
+
+| Param | Type | Description |
+| --- | --- | --- |
+| request | <code>express</code> | an instance of the app wrapped in supertest |
+
+<a name="Page+visit"></a>
+
+### page.visit(path) ⇒ <code>Promise</code>
+Page.prototype.visit - get a jsdom instance with jquery returned as a promise
+
+**Kind**: instance method of <code>[Page](#Page)</code>  
+**Returns**: <code>Promise</code> - a promise containing a jsdom object  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| path | <code>string</code> | the relative path |
+
+<a name="Page+validate"></a>
+
+### page.validate($) ⇒ <code>Promise</code>
+Page.prototype.validate - A promise that validates the page against w3c standards
+
+**Kind**: instance method of <code>[Page](#Page)</code>  
+**Returns**: <code>Promise</code> - resolves the promise with the jsdom instance if there are no errors found, otherwise rejects with an object containing the errors  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| $ | <code>jsdom</code> | an instance of jsdom |
+
+<a name="Page+get"></a>
+
+### page.get(path) ⇒ <code>Promise</code>
+Page.prototype.get - Sends a GET request to the path and resolves a promise with that object
+
+**Kind**: instance method of <code>[Page](#Page)</code>  
+**Returns**: <code>Promise</code> - resolves the promise with the response object, otherwise rejects it with an error  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| path | <code>string</code> | the path |
+
+<a name="Page+post"></a>
+
+### page.post(path, controls) ⇒ <code>type</code>
+Page.prototype.post - Sends a post request with the data provided. This is typically used in conjunction with the clickButton method.
+
+**Kind**: instance method of <code>[Page](#Page)</code>  
+**Returns**: <code>type</code> - Resolves with a request object  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| path | <code>type</code> | The path to send |
+| controls | <code>type</code> | an object formatted as an array of objects controls containing a name and value. jQuery's .serializeArray function will format controls properly. |
+
+<a name="Page+clickLink"></a>
+
+### page.clickLink(text) ⇒ <code>Promise</code>
+Page.prototype.clickLink - A thenable that visits a link by loading the href gotten by the text of the link.
+
+**Kind**: instance method of <code>[Page](#Page)</code>  
+**Returns**: <code>Promise</code> - Returns the resolve or reject value of the promise returned by the visit method  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| text | <code>string</code> | The text of the link to be clicked |
+
+<a name="Page+fillIn"></a>
+
+### page.fillIn(text, options) ⇒ <code>function</code>
+Page.prototype.fillIn - Fills in an input or textarea that is attached to a label
+
+**Kind**: instance method of <code>[Page](#Page)</code>  
+**Returns**: <code>function</code> - returns a function usable in a promise chain  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| text | <code>string</code> | the visible content of the label attached to the input via a `for` |
+| options | <code>object</code> | the content - a string will be converted to an object with the format: { 'with': str } |
+
+<a name="Page+check"></a>
+
+### page.check(text) ⇒ <code>function</code>
+Page.prototype.check - Checks a checkbox given the text of the checkbox
+
+**Kind**: instance method of <code>[Page](#Page)</code>  
+**Returns**: <code>function</code> - A function usable in a promise chain  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| text | <code>string</code> | The visible content of the label associated with the checkbox |
+
+<a name="Page+clickButton"></a>
+
+### page.clickButton(text) ⇒ <code>Promise</code>
+Page.prototype.clickButton - Submits a form by gathering form values and creating a post request
+
+**Kind**: instance method of <code>[Page](#Page)</code>  
+**Returns**: <code>Promise</code> - Returns a promise from the Post method  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| text | <code>string</code> | The visible text of the submit button |
+
+<a name="Page+select"></a>
+
+### page.select(text, options) ⇒ <code>function</code>
+Page.prototype.select - Selects an option from a select box based on the text of the option
+
+**Kind**: instance method of <code>[Page](#Page)</code>  
+**Returns**: <code>function</code> - a function usable in a promise chain  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| text | <code>string</code> | The visible text of the label associated with the select box |
+| options | <code>object</code> | an object to configure the selection -  - a string will be converted to an object with the format: { 'from': str } |
 
 
 ## Example
@@ -43,15 +167,15 @@ const server = require('../mailMerge/app');
 const request = require('supertest')(server);
 
 describe("POST /", () => {
-  it("When a user fills in the form and submits it, the values they filled in the form fields should remain filled in.", (done) => {
-    let page = createPage(request);
+  it("When a user fills in the form and submits it, Then the values they filled in the form fields should remain filled-in", (done) => {
+    let page = new Page(request)
 
-    let recipTestVal = "Jean Luc,Picard,jeanluc.picard@federation.gov \r\n William,Riker,william.riker@federation.gov";
+    let recipTestVal = "Jean Luc,Picard,jeanluc.picard@federation.gov \r\n James,Riker,james.riker@federation.gov";
     let subjectTestVal = "Email Activation for #{first} #{last}";
-    let bodyTestVal = "Hello Officer #{last}, your email: #{email} has been activated. Welcome to the federation!";
+    let bodyTestVal = "Hello Officer #{last}, your email: #{email} has been activated";
 
     return page.visit('/')
-    .then(page.validate())
+    .then(page.validate)
     .then(page.fillIn('Contacts', recipTestVal))
     .then(page.fillIn('Subject', subjectTestVal))
     .then(page.fillIn('Body', bodyTestVal))
@@ -64,8 +188,9 @@ describe("POST /", () => {
 
       done();
 
+    }).catch(function (err) {
+      throw new Error(err);
     });
   });
-});
 
 ```
--- index.js
+++ index.js
@@ -4,138 +4,200 @@ const jsdom = require('jsdom')
 const Promise = require("bluebird");
 Promise.promisifyAll(jsdom);
 
-global.createPage = createPage
+global.Page = Page
 global.jsdom = jsdom
 
-function createPage(request) {
 
-  return {
-    visit,
-    get,
-    post,
-    validate,
-    clickLink,
-    clickButton,
-    fillIn,
-    select,
-    check,
-  }
+/**
+ * Represents a Page.
+ * @constructor
+ * @param {express} request - an instance of the app wrapped in supertest
+ */
+function Page(request) {
+  this.request = request;
+}
 
-  function visit(path) {
-    this.path = path
-    return get(path).then(function (html) {
-      return jsdom.envAsync(html, ["http://code.jquery.com/jquery.js"])
+
+/**
+ * Page.prototype.visit - get a jsdom instance with jquery returned as a promise
+ *
+ * @param  {string} path the relative path
+ * @return {Promise}      a promise containing a jsdom object
+ */
+Page.prototype.visit = function(path) {
+  this.path = path
+  return this.get(path).then((res) => {
+    return jsdom.envAsync(res.text, ["http://code.jquery.com/jquery.js"]).then((window) => {
+      this.window = window;
+      return window.$
     })
-  }
+  })
+}
 
-  function validate(input) {
-    return new Promise((resolve, reject) => {
-      w3cjs.validate({
-        input,
-        callback: function (validationResponse) {
-          if (validationResponse.messages.length > 0 ) {
-            reject({error: 'html errors have been found', results: validationResponse})
-          };
-          resolve(input)
-        }
-      })
+/**
+ * Page.prototype.validate - A promise that validates the page against w3c standards
+ *
+ * @param  {jsdom} $          an instance of jsdom
+ * @return {Promise} resolves the promise with the jsdom instance if there are no errors found, otherwise rejects with an object containing the errors
+ */
+Page.prototype.validate = function($) {
+  return new Promise((resolve, reject) => {
+    w3cjs.validate({
+      input: this.response.text,
+      callback: (validationResponse) => {
+        if (validationResponse.messages.length > 0 ) {
+          reject({error: 'html errors have been found', results: validationResponse})
+        };
+        resolve($)
+      }
     })
-  }
+  })
+}
 
-  function get(path) {
-    return new Promise((resolve, reject) => {
-      request
-      .get(path)
-      .expect(200)
-      .end(function(err, res) {
+/**
+ * Page.prototype.get - Sends a GET request to the path and resolves a promise with that object
+ *
+ * @param  {string} path      the path
+ * @return {Promise} resolves the promise with the response object, otherwise rejects it with an error
+ */
+Page.prototype.get = function(path) {
+  return new Promise((resolve, reject) => {
+    this.request
+    .get(path)
+    .expect(200)
+    .end((err, res) => {
+      if (err) reject(err);
+      this.response = res;
+      resolve(res);
+    })
+  })
+}
+
+/**
+ * Page.prototype.post - Sends a post request with the data provided. This is typically used in conjunction with the clickButton method.
+ *
+ * @param  {string} path     The path to send
+ * @param  {object} controls an object formatted as an array of objects controls containing a name and value. jQuery's .serializeArray function will format controls properly.
+ * @return {Promise}          Resolves with a request object
+ */
+Page.prototype.post = function(path, controls) {
+  return new Promise((resolve, reject) => {
+    let thisRequest = this.request.post(path)
+    thisRequest.type('form')
+
+    controls.forEach((control) => {
+      thisRequest.send(control.name+"="+control.value)
+    })
+
+    thisRequest
+      .end((err, res) => {
         if (err) reject(err);
-        resolve(res.text)
+        this.response = res;
+        resolve(res)
+      })
+  })
+}
+
+/**
+ * Page.prototype.clickLink - A thenable that visits a link by loading the href gotten by the text of the link.
+ *
+ * @param  {string} text The text of the link to be clicked
+ * @return {Promise}      Returns the resolve or reject value of the promise returned by the visit method
+ */
+Page.prototype.clickLink = function(text) {
+  return ($) => {
+    let $link = $(`a:contains("${text}")`)
+    return this.visit($link.attr('href'))
+  }
+}
+
+/**
+ * Page.prototype.fillIn - Fills in an input or textarea that is attached to a label
+ *
+ * @param  {string} text   the visible content of the label attached to the input via a `for`
+ * @param  {object} options the content - a string will be converted to an object with the format: { 'with': str }
+ * @return {Function} returns a function usable in a promise chain
+ */
+Page.prototype.fillIn = function(text, options) {
+  if (typeof options === 'string') options = { with: options };
+  return ($) => {
+    let $label = $(`label:contains("${text}")`)
+    let $input = $(`input#${$label.attr('for')}`)
+    $input.val(options.with)
+    if ($input.length == 0) {
+      let $input = $(`textarea#${$label.attr('for')}`)
+      $input.html(options.with)
+    }
+    return $
+  }
+}
+
+/**
+ * Page.prototype.check - Checks a checkbox given the text of the checkbox
+ *
+ * @param  {string} text The visible content of the label associated with the checkbox
+ * @return {function}      A function usable in a promise chain
+ */
+Page.prototype.check = function(text) {
+  return ($) => {
+    let $label = $(`label:contains("${text}")`)
+    let $control
+    if ($label.attr('for')) {
+      $control = $(`input#${$label.attr('for')}`)
+    } else {
+      $control = $label.find(`input`)
+    }
+    if (!$control.val()) $control.val('on')
+    $control.prop('checked', true).attr('checked', 'checked')
+    return $
+  }
+}
+
+/**
+ * Page.prototype.clickButton - Submits a form by gathering form values and creating a post request
+ *
+ * @param  {string} text The visible text of the submit button
+ * @return {Promise}      Returns a promise from the Post method
+ */
+Page.prototype.clickButton = function(text) {
+  return ($) => {
+    let $button = $(`input[type=submit][value="${text}"]`)
+    if (!$button.length) {
+      $button = $(`button:contains("${text}")`)
+    }
+    let $form = $button.closest('form')
+
+    let path = $form.attr('action')
+    return this.post(path, $form.serializeArray()).then(function (res) {
+      if (res.status === 302) return visit(res.headers.location)
+      this.response = res;
+      return jsdom.envAsync(res.text, ["http://code.jquery.com/jquery.js"]).then((window) => {
+        this.window = window;
+        return window.$;
       })
     })
   }
+}
 
-  function post(path, controls) {
-    return new Promise((resolve, reject) => {
-      let thisRequest = request.post(path)
-      thisRequest.type('form')
-
-      controls.forEach((control) => {
-        thisRequest.send(control.name+"="+control.value)
-      })
-
-      thisRequest
-        .end(function(err, res) {
-          if (err) reject(err);
-          resolve(res)
-        })
-    })
-  }
-
-  function clickLink(text) {
-    return function ($) {
-      let $link = $(`a:contains("${text}")`)
-      return visit($link.attr('href'))
+/**
+ * Page.prototype.select - Selects an option from a select box based on the text of the option
+ *
+ * @param  {string} text  The visible text of the label associated with the select box
+ * @param  {object} options  an object to configure the selection -  - a string will be converted to an object with the format: { 'from': str }
+ * @return {function}         a function usable in a promise chain
+ */
+Page.prototype.select = function(text, options) {
+  return ($) => {
+    if (typeof options === 'string') options = {'from' : options};
+    let $label = $(`label:contains("${options.from}")`)
+    let $select
+    if ($label.attr('for')) {
+      $select = $(`select#${$label.attr('for')}`)
+    } else {
+      $select = $label.find(`select`)
     }
+    $select.find('option').removeAttr('selected')
+    $select.find(`option:contains(${text})`).attr('selected', 'selected');
+    return $
   }
-
-  function fillIn(text, options) {
-    if (typeof options === 'string') options = { with: options };
-    return function ($) {
-      let $label = $(`label:contains("${text}")`)
-      let $input = $(`input#${$label.attr('for')}`)
-      $input.val(options.with)
-      if ($input.length == 0) {
-        let $input = $(`textarea#${$label.attr('for')}`)
-        $input.html(options.with)
-      }
-      return $
-    }
-  }
-
-  function check(text) {
-    return function ($) {
-      let $label = $(`label:contains("${text}")`)
-      let $control
-      if ($label.attr('for')) {
-        $control = $(`input#${$label.attr('for')}`)
-      } else {
-        $control = $label.find(`input`)
-      }
-      if (!$control.val()) $control.val('on')
-      $control.prop('checked', true).attr('checked', 'checked')
-      return $
-    }
-  }
-
-  function clickButton(text) {
-    return function ($) {
-      let $button = $(`input[type=submit][value="${text}"]`)
-      if (!$button.length) {
-        $button = $(`button:contains("${text}")`)
-      }
-      let $form = $button.closest('form')
-
-      let path = $form.attr('action')
-      return post(path, $form.serializeArray()).then(function (res) {
-        if (res.status === 302) return visit(res.headers.location)
-        return jsdom.load(res.text)
-      })
-    }
-  }
-
-  function select(text, options) {
-    return function ($) {
-      let $label = $(`label:contains("${options.from}")`)
-      let $select
-      if ($label.attr('for')) {
-        $select = $(`select#${$label.attr('for')}`)
-      } else {
-        $select = $label.find(`select`)
-      }
-      $select.find('option').removeAttr('selected')
-      $select.find(`option:contains(${text})`).attr('selected', 'selected');
-      return $
-    }
-  }
-
 }
